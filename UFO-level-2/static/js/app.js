176ebf1797cbf12277c93fa942f7fe36a31e3930
Ava Wiegert <aemily6296@gmail.com>
2021-02-23 19:19:42 -0600

    finished level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -30,14 +30,42 @@ function filterData() {
     d3.event.preventDefault();
     
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    var inputElement_date = d3.select("#datetime");
+    var inputElement_city = d3.select("#city");
+    var inputElement_state = d3.select("#state");
+    var inputElement_country = d3.select("#country");
+    var inputElement_shape = d3.select("#shape");
   
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    var inputValue_date = inputElement_date.property("value");
+    var inputValue_city = inputElement_city.property("value");
+    var inputValue_state = inputElement_state.property("value");
+    var inputValue_country = inputElement_country.property("value");
+    var inputValue_shape = inputElement_shape.property("value");
   
     // fitler the data
-    var filteredData = tableData.filter(signting => signting.datetime === inputValue);
-  
+    var filteredData = tableData
+
+    if (inputValue_date !== null && inputValue_date !== '') {
+        filteredData= filteredData.filter(signting => signting.datetime === inputValue_date);
+    };
+
+    if (inputValue_city !== null && inputValue_city !== '') {
+        filteredData = filteredData.filter(signting => signting.city === inputValue_city);
+    };
+
+    if (inputValue_state !== null && inputValue_state !== '') {
+        filteredData = filteredData.filter(signting => signting.state === inputValue_state);
+    };
+
+    if (inputValue_country !== null && inputValue_country !== '') {
+        filteredData = filteredData.filter(signting => signting.country === inputValue_country);
+    };
+
+    if (inputValue_shape !== null && inputValue_shape !== '') {
+        filteredData = filteredData.filter(signting => signting.shape === inputValue_shape);
+    };
+
     // // select the table
     // var tbody = d3.select("tbody");
   
